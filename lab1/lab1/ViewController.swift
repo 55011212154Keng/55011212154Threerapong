61d858059f822b8806d470abf5d927755d9c094e
55011212154Threerapong <student@tests-iMac-3.local>
2014-09-26 16:55:02 +0700

    edit4
    edit5

---
+++ lab1/lab1/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  lab1
+//
+//  Created by Student on 9/26/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    
+    @IBOutlet weak var NexusTable: UITableView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
